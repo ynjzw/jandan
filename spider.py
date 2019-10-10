cb2d04ae827921f8f741dafae3ed70a93c4fef98
ynjzw <55905855+ynjzw@users.noreply.github.com>
2019-10-10 22:37:01 +0800

    Update spider.py

--- spider.py
+++ spider.py
@@ -2,31 +2,31 @@ from lxml import html
 import requests
 import cx_Oracle
 
-#连接数据库
-db = cx_Oracle.connect('tom/lintao@localhost/orcl')
-c = db.cursor()
 
 #获取煎蛋网首页HTML文档
 html_doc=requests.get('http://jandan.net').text
 selector=html.fromstring(html_doc)
 ul_list=selector.xpath("//div[@class='indexs']")
 
-#爬取首页24篇文章的链接，主标题，副标题，并保存数据库
+#爬取首页24篇文章的链接url，主标题title，副标题info
 for li in ul_list:
 	urls=li.xpath("h2/a/@href")
-	str1=''.join(urls)
-	title=li.xpath("h2/a/text()")
-	str2 = ''.join(title)
-	info=li.xpath("text()")
-	str=info[-1].replace('\r\n\t', '')
-	str3=str.replace('\t','')
-	
-	sql="insert into jandan (url,title,info) values ('%s','%s','%s')" % (str1,str2,str3)
-	c.execute(sql)
+	url=''.join(urls)
+	titles=li.xpath("h2/a/text()")
+	title= ''.join(title)
+	infos=li.xpath("text()")
+	infomation=infos[-1].replace('\r\n\t', '')
+	info=infomation.replace('\t','')
 
-r=c.execute("select * from jandan")
-for i in r.fetchall():
-	print(i)
+        #爬取对应文章下的评论comment,评论者commentator
+        html_doc=requests.get(url).text
+	s=html.fromstring(html_doc)
+        cm_list=s.xpath("//ol[@class='commentlist']/li")
+	for cm in cm_list:
+		commentators=cm.xpath("//div[@class='author']/strong/text()")
+		comments=cm.xpath("//div[@class='text']/p/text()")
+		for commentator in commentators:
+			print(commentator )
 
-db.close()
-
+		for comment in comments:
+			print(comment )
